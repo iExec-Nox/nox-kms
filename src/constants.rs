6977a9ed0d2a8eb9709557046d1ce86a9f84227b
aghiles-ait <aghiles.ait-messaoud@iex.ec>
2026-01-16 10:28:21 +0100

    refactor: assign direct value to constant

--- src/constants.rs
+++ src/constants.rs
@@ -2,5 +2,5 @@ use k256::ProjectivePoint;
 
 pub const KEY_FILE_SIZE: usize = 65;
 pub const G: ProjectivePoint = ProjectivePoint::GENERATOR;
-pub const MIN_RSA_KEY_HEX_LEN: usize = 2 * 256; // 2*256 bytes = 2048 bits = 512 hex chars
+pub const MIN_RSA_KEY_HEX_LEN: usize = 512; // 256 bytes (RSA-2048 modulus) = 512 hex chars
 pub const EXPECTED_EPHEMERAL_PUB_KEY_HEX_LEN: usize = 66; // 33 bytes = 66 hex chars
