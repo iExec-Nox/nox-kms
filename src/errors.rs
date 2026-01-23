004e078711e8f9085312697e9bebfbd29fd3cd2e
aghiles-ait <aghiles.ait-messaoud@iex.ec>
2026-01-23 15:44:52 +0100

    fix: add a warning message

--- src/errors.rs
+++ src/errors.rs
@@ -1,6 +1,7 @@
 use axum::{Json, http::StatusCode, response::IntoResponse};
 use serde_json::json;
 use thiserror::Error;
+use tracing::warn;
 
 #[derive(Error, Debug)]
 pub enum KmsError {
@@ -12,6 +13,7 @@ pub enum KmsError {
 
 impl IntoResponse for KmsError {
     fn into_response(self) -> axum::response::Response {
+        warn!("Request failed: {}", self);
         (
             StatusCode::BAD_REQUEST,
             Json(json!({ "error": self.to_string() })),
