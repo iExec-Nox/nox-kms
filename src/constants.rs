fd08ee2ed03fae97f2d0c43569c3691543c15438
aghiles-ait <106151923+aghiles-ait@users.noreply.github.com>
2026-01-30 11:12:51 +0100

    feat: check gateway authorization (#13)

--- src/constants.rs
+++ src/constants.rs
@@ -4,5 +4,7 @@ pub const KEY_FILE_SIZE: usize = 65;
 pub const G: ProjectivePoint = ProjectivePoint::GENERATOR;
 pub const MIN_RSA_KEY_HEX_LEN: usize = 512; // 256 bytes (RSA-2048 modulus) = 512 hex chars
 pub const EXPECTED_EPHEMERAL_PUB_KEY_HEX_LEN: usize = 66; // 33 bytes = 66 hex chars
-// EIP-712 domain name for PublicKeyProof generation
+// EIP-712 constants
+pub const EIP_712_DOMAIN_VERSION: &str = "1";
 pub const PROTOCOL_PUBLIC_KEY_EIP712_DOMAIN_NAME: &str = "ProtocolPublicKey";
+pub const PROTOCOL_DELEGATE_EIP712_DOMAIN_NAME: &str = "ProtocolDelegate";
